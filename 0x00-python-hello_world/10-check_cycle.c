7ce4281028a6b5647ff5184ff6dc524b9a35c470
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-07 07:57:01 -0700

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *list1 = list;
+	listint_t *check = list;
+
+	if (!list)
+		return (0);
+
+	while(list1 & check && check->next)
+	{
+		list11 = list1->next;
+		check = check->next->next;
+		if (list1 == check)
+			return (1);
+	}
+	return (0);
+}
