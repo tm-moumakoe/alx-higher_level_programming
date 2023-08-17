1e3eb92c1a17a21a3864803da5139857ff756ac1
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-16 19:20:13 -0700

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (not roman_string) or (not isinstance(roman_string, str)):
+        return 0
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    r_last = 0
+    n_list = [0]
+
+    for ch in roman_string:
+        for r_num in r_dict:
+            if r_num == ch:
+                if r_dict.get(ch) <= r_last:
+                    num += r_sub(n_list)
+                    n_list = [r_dict.get(ch)]
+                else:
+                    n_list.append(r_dict.get(ch))
+                r_last = r_dict.get(ch)
+    num += r_sub(n_list)
+    return num
+
+def r_sub(n_list):
+    n_sub = 0
+    m_list = max(n_list)
+
+    for n in n_list:
+        if m_list > n:
+            n_sub += n
+    return m_list - n_sub
