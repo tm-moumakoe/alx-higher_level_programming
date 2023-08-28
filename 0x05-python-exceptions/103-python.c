bd3b3ad04a8999ba80e635da884abe39670e7469
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-28 13:03:22 -0700

    task 10 mod

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,34 +1,9 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
 
-/**
- * print_python_bytes - gives data of the PyBytesObject
- * @p: the PyObject
- */
-void print_python_bytes(PyObject *p)
-{
-	Py_ssize_t size = 0, i = 0;
-	char *str = NULL;
-
-	fflush(stdout);
-	printf("[.] bytes object info\n");
-	if (!PyBytes_CheckExact(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-	size = PyBytes_Size(p);
-	printf("  size: %zd\n", size);
-	str = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
-	printf("  trying str: %s\n", str);
-	printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
-	while (i < size + 1 && i < 10)
-	{
-		printf(" %02hhx", str[i]);
-		i++;
-	}
-	printf("\n");
-}
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
 /**
  * print_python_float - gives data of the PyFloatObject
@@ -48,8 +23,37 @@ void print_python_float(PyObject *p)
 		return;
 	}
 	val = ((PyFloatObject *)p)->ob_fval;
-	str = PyOS_double_to_str(val, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  val: %s\n", str);
+	str = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", str);
+}
+
+/**
+ * print_python_bytes - gives data of the PyBytesObject
+ * @p: the PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t len = 0, i = 0;
+	char *str = NULL;
+
+	fflush(stdout);
+	printf("[.] bytes object info\n");
+	if (!PyBytes_CheckExact(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	len = PyBytes_Size(p);
+	printf("  size: %zd\n", size);
+	str = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
+	printf("  trying string: %s\n", str);
+	printf("  first %zd bytes:", len < 10 ? len + 1 : 10);
+	while (i < len + 1 && i < 10)
+	{
+		printf(" %02hhx", str[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -58,7 +62,7 @@ void print_python_float(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size = 0;
+	Py_ssize_t len = 0;
 	PyObject *obj;
 	int i = 0;
 
@@ -66,10 +70,10 @@ void print_python_list(PyObject *p)
 	printf("[*] Python list info\n");
 	if (PyList_CheckExact(p))
 	{
-		size = PyList_GET_SIZE(p);
-		printf("[*] Size of the Python List = %zd\n", size);
+		len = PyList_GET_SIZE(p);
+		printf("[*] Size of the Python List = %zd\n", len);
 		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-		while (i < size)
+		while (i < len)
 		{
 			obj = PyList_GET_ITEM(p, i);
 			printf("Element %d: %s\n", i, obj->ob_type->tp_name);
