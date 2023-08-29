d7e662307830bd9dcda711273976892275bde884
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-29 16:15:45 -0700

    task 2 mod

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,6 +7,11 @@ class Square:
      def __init__(self, size=0):
         """ Initialzes a Sqaure instance
         Args:
-            size (int): size of the square.
+            size (int): size of the square
         """
-        self.__size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
