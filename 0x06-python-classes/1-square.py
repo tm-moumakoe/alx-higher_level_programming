13898e93027451c28826b27300ab962ab24bd284
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-29 15:32:05 -0700

    tasl 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defins a Square """
+
+
+class Square:
+    """ A square with private instance attribute size """
+    def __init__(self, size):
+        """ Initializes Square class """
+        self.__size = size
