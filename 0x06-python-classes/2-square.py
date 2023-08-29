43fb2539f63f83bb3b65bbbe11c0405b10fc0958
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-29 16:42:31 -0700

    task 2 mod 6

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,7 +16,7 @@ class Square:
         Returns:
             None
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
