cb0f84cc3093adb8e586035d637ea01d83888437
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-08-29 16:48:52 -0700

    task 2 mod 7

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-# 2-square.py by TM Moumakoe
-""" A module that defines a Square """
+""" Defines a Square """
 
 
 class Square:
-    """ Represents a Square
-
-    Attributes:
-        __size (int): size of a Square
-    """
-    def __init__(self, size=0):
-        """ Initializes the Sqaure class
+    """ A square with private instance attribute size """
+    def __init__(self, size):
+        """ Initializes Square class
         Args:
-            size (int): size of a Square
-        Returns:
-            None
+            size (int): size of defined Square
         """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError('size must be >= 0')
-        
+            raise ValueError('soze must be >= 0')
         self.__size = size
