5d2ee875387552d864f51e5881ebcaa4128f1a24
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-09-04 14:37:24 -0700

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# 1-rectangle.py by TM Moumakoe
+"""Defines a Rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle
+
+        Args:
+            width (int): width of the new rectangle.
+            height (int): height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if not isinstance(w, int):
+            raise TypeError("width must be an integer")
+        if w < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = w
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        if not isinstance(h, int):
+            raise TypeError("height must be an integer")
+        if h < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = h
