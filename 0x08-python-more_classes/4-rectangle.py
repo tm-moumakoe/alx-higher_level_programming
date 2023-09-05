f91f9df4f7071122298a90d5e017d77116d1b6ea
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-09-04 18:16:36 -0700

    task 4 mod

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,74 +1,71 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""defines a rectangle"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
+        """Initializes a rectangle
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
         """
         self.width = width
         self.height = height
 
-    def __str__(self):
-        """Return the printable representation of the Rectangle.
-
-        Represents the rectangle with the # character.
-        """
+    def __str__(self) -> str:
+        """presents a diagram of the rectangle defined for an object"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
 
     def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return (rect)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """retrieves width attribute"""
         return self.__width
 
     @width.setter
-    def width(self, w):
-        if not isinstance(w, int):
+    def width(self, value):
+        """sets width attribute"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if w < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = w
+        self.__width = value
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """retrieves height attribute"""
         return self.__height
 
     @height.setter
-    def height(self, h):
-        if not isinstance(h, int):
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if h < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = h
+        self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """Returns the area of the rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
+        """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
