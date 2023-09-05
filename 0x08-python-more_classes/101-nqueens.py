08ced8393776a7a6dc53a7ffe19fd41f1c681316
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-09-04 19:03:16 -0700

    task 10 mod 3

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -23,11 +23,13 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
 
 def xout(board, row, col):
     """X out spots on a chessboard.
@@ -79,6 +81,7 @@ def xout(board, row, col):
         board[r][c] = "x"
         c -= 1
 
+
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
@@ -88,6 +91,7 @@ def get_solution(board):
                 solution.append([r, c])
                 break
     return (solution)
+
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
@@ -110,6 +114,7 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
     return (solutions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
