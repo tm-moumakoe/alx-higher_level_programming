76fc08e7d4eda5b0d23c1fff43f2810e490a19b5
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-09-05 18:08:36 -0700

    0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a LockedClass"""
+
+
+class LockedClass:
+    """Represents a LockedClass
+    Attributes:
+        first_name (str): object's first name
+    """
+
+    __slots__ = ["first_name"]
