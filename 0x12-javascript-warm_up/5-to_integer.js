71829fd077d0fb49fdda8b977a9e686465838e09
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-13 18:24:05 -0800

    task 5 mod

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (isNaN(argv[2]) || argv[2] === undefined) console.log('Not a number');
-else console.log('My number:', parseInt(argv[2]));
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) console.log('Not a number');
+else console.log('My number:', parseInt(process.argv[2]));
