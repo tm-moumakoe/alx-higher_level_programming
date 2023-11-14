a784f364d804da404be05add150c89c7b3e9342c
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-13 18:17:57 -0800

    task 3 mod

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv[2]) console.log(`${argv[2]}`);
+if (process.argv[2]) console.log(`${argv[2]}`);
 else console.log('No argument');
