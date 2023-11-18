a97f426338d4e295a937888f31e095dac67778da
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-18 10:46:39 -0800

    task 1 mod and tasks 2-5

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with `N` from hbtn_0e_0_usa using MySQL"""
+"""Lists states from the database hbtn_0e_0_usa that match arguments"""
 import sys
 import MySQLdb
 
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name
-            LIKE BINARY 'N%' ORDER BY states.id""")
+    match = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Lists all states starting with `N` from hbtn_0e_0_usa using MySQL"""
-import sys
+"""Lists all cities from the database hbtn_0e_0_usa using MySQL"""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name
-            LIKE BINARY 'N%' ORDER BY states.id""")
+    cursor.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
