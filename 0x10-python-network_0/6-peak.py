5eae43e45c11e4e72a7802e922c0adef1b8430e2
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-30 15:13:12 -0800

    task 6 mod 1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,22 @@
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """Finds a peak in the list."""
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if (mid == 0 or list_of_integers[mid - 1]
+            <= list_of_integers[mid]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1]
+                <= list_of_integers[mid]):
+            return list_of_integers[mid]
+
+        if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
