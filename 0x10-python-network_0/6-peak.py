2286cca30bbaa10b335efbb7739fe4caba4601c1
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-30 14:42:44 -0800

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""finds peak in list of ints"""
+
+
+def find_peak(list_of_integers):
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
