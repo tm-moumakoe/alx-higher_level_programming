0c33a3e76ed38bb2b719cb8aff6919cac86af6d5
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-30 15:17:46 -0800

    task 9 mod

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# causes the server to respond with a message "You got me!"
-curl -s -d "@$2" -H "Content-Type: application/json" -X POST "$1"
+# causes the server to respond with a message containing "You got me!"
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
